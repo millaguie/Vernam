aac585d95d19f060b7a5ca771f308406cf393f66
Javier Picado Ladrón de Guevara <millaguie@gmail.com>
2017-04-24 19:16:56 +0200

    (+) Some more key management

--- vernam/keymanagement.py
+++ vernam/keymanagement.py
@@ -17,7 +17,7 @@ def catalog(keyPath):
     if not os.path.exists(keyPath):
         sys.exit("Could not find key {}".format(keyPath))
     if os.path.exists(keyPath+".yaml"):
-        sys.exit("A description finle already exists, won't create a new one")
+        sys.exit("A description file already exists, won't create a new one")
     genUUID=uuid.uuid4()
     print("Generating hash of key, this might take some time")
     BLOCKSIZE = 65536
@@ -34,3 +34,53 @@ def catalog(keyPath):
                     "keyfile: {}\n".format(fileName) +
                     "UUID: {}\n".format(str(genUUID)) +
                     "sha512: {}".format(hashSum))
+
+
+def checkCatalogUUID(keyPath, binaryUUID=None, asciiUUID=None):
+    """
+    Function to check key UUID and UUID used in message, returns True or False
+
+     Parameters
+    -----------
+    keyPath     : path to the file used as key
+    binaryUUID  : optional, message UUID in binary format
+    asciiUUID   : optional, message UUID in string format
+    """
+    if not os.path.exists(keyPath):
+        sys.exit("Could not find key {}".format(keyPath))
+    if not os.path.exists(keyPath+".yaml"):
+        sys.exit("Could not find decriptor for key, please catalog it before")
+
+    keyConfig = {}
+    keyConfig = yaml.load(open(keyPath+".yaml",'r'))
+    confUUID = uuid.UUID("urn:uuid:"+keyConfig["UUID"])
+    if asciiUUID is not None:
+        messageUUID = uuid.UUID(asciiUUID)
+    elif binaryUUID is not None:
+        messageUUID = uuid.UUID(binaryUUID)
+    else:
+        return False
+    if messageUUID.int == confUUID.int:
+        return True
+    else:
+        return False
+
+
+def getCatalogUUID(keyPath):
+    """
+    Function to get UUID from key config file, returns an UUID object.
+
+     Parameters
+    -----------
+    keyPath     : path to the file used as key
+
+    """
+
+    if not os.path.exists(keyPath):
+        sys.exit("Could not find key {}".format(keyPath))
+    if not os.path.exists(keyPath+".yaml"):
+        sys.exit("Could not find decriptor for key, please catalog it before")
+
+    keyConfig = {}
+    keyConfig = yaml.load(open(keyPath+".yaml",'r'))
+    return uuid.UUID("urn:uuid:"+keyConfig["UUID"])

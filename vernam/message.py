beec1043801fbefb2bd66dcaa95428a26b2f968d
Javier Picado Ladrón de Guevara <millaguie@gmail.com>
2017-04-26 20:49:31 +0200

    (+) adds properly manage message files

--- vernam/message.py
+++ vernam/message.py
@@ -5,6 +5,8 @@ import hashlib
 import keymanagement
 import array
 from struct import pack
+from struct import unpack
+from util import hashSum
 
 global L2RHEADER
 L2RHEADER = bytearray([222, 210, 7, 163, 100]);
@@ -16,23 +18,68 @@ max_int64 = 0xFFFFFFFFFFFFFFFF
 
 
 def readMessage(keyPath, messagePath):
+        """
+        This function reads a message (envelope) in the defined format, and
+        returns the data inside the file, offset in key file and the raw data.
+        It checks that message key UUID matchs defined key UUID, also checks
+        consistency of message via sha512
+
+         Parameters
+        -----------
+        keyPath     : path to the file used as key
+        messagePath : path to the file used to read the message
+        """
+    keyUUID = keymanagement.getCatalogUUID(keyPath)
+
+    with open(messagePath, "rb") as file:
+        header = unpack(">iiiii", file.read(5*4))
+        print(header)
+        msgSize = unpack(">Q",file.read(8))[0]
+        print(msgSize)
+        offsetInKey = unpack(">Q",file.read(8))
+        #Hay que convertir los dos enteros de 64 bits en uno de 128
+        msgKeyUUID1, msgKeyUUID2 = unpack(">QQ",file.read(16))
+        print(type(msgKeyUUID1))
+        msgKeyUUID = (msgKeyUUID1 << 64) | msgKeyUUID2
+        if keyUUID.int != msgKeyUUID:
+            raise ValueError("Bad Key UUID")
+        message = unpack(">{}s".format(msgSize), file.read(msgSize))[0]
+        print(message)
+        msgFileHash = file.read()
+        if hashSum(message) != msgFileHash.encode("hex"):
+            raise ValueError("Failed to hash message ")
+        return offsetInKey, message
 
 
-
-    if not os.path.exists(keyPath):
-        sys.exit("Could not find key {}".format(keyPath))
-        # a, b     = struct.unpack('>QQ', packed)
-        # unpacked = (a << 64) | b
 
 
 def writeMessage(keyPath, messagePath, ciphered, offsetInKey, leftToRigth=True):
+    """
+    This function Writes a message in the defined format.
+    Format of the message is as follows
+    * Header 20 bytes, as defined, two options, one for R2L and another for L2R
+      it's on the todo.
+    * Message size in 8 bytes (64 bits) integer
+    * Key UUID used in message 16 bytes
+    * Message it's self, it's size is defined in the second field
+    * Hash of the message, 32 bytes a sha512
 
-    print(keyPath)
+     Parameters
+    -----------
+    keyPath     : path to the file used as key
+    messagePath : path to the file used to store the message
+    ciphered    : ciphered data to write in the file (envelope)
+    offsetInKey : need to jump to this byte in key to decrypt
+    leftToRigth : Indicates if the key will need to be readed R2L or L2R (true)
+    """
+
     keyUUID = keymanagement.getCatalogUUID(keyPath)
-    msgSize = ciphered.count(ciphered)
+    msgSize = len(ciphered)
+    print(msgSize)
+    print(ciphered)
     with open(messagePath, "wb") as file:
         # Write file header right to left or left to right
-        file.write(pack(">IIIII", *R2LHEADER))
+        file.write(pack(">iiiii", *R2LHEADER))
         # Write menssage size in bytes
         file.write(pack(">Q",msgSize))
         # Write offset in key to decrypt message
@@ -41,15 +88,9 @@ def writeMessage(keyPath, messagePath, ciphered, offsetInKey, leftToRigth=True):
         file.write(pack('>QQ', (keyUUID.int >> 64) & max_int64,
                         keyUUID.int & max_int64))
         #write message it's self
-        file.write(pack(">I%ds" % (msgSize,), msgSize, ciphered))
-        # Prpeare hash for the message
-        hasher = hashlib.sha512()
-        for b in ciphered:
-            hasher.update(b)
-        msgHash = hasher.hexdigest()
-
-        #Write message hash
-        print(msgHash)
+        file.write(pack(">{}s".format(msgSize), ciphered))
+        # Get hash for the message
+        msgHash = hashSum(ciphered)
         msgHashint = msgHash.decode("hex")
         msgHashArray = bytearray(msgHashint)
         hashSize = msgHashArray.count(msgHashArray)
---
+++ vernam/util.py
@@ -0,0 +1,17 @@
+import hashlib
+
+
+def hashSum(data):
+    """
+    This function helps to create hash resumes for many parts of the code. It
+    uses sha512 as hashing algorithm
+
+     parameters
+    -----------
+
+    data : data to be hashsed
+    """
+    hasher = hashlib.sha512()
+    for b in data:
+        hasher.update(b)
+    return hasher.hexdigest()

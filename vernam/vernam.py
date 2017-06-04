8ea2111c7d939ce63e83354759f6b1b4ada105e8
Javier Picado Ladrón de Guevara <millaguie@gmail.com>
2017-06-04 15:02:46 +0200

    FIX some readability and style issues in vernam.py

--- vernam/vernam.py
+++ vernam/vernam.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
+"""
+This is the core class form verna encryption. It manages all
+Vernam encryption/decription stuff
+"""
 import sys
 import os
+import math
 from lz4 import compressHC, uncompress
 import tempfile
-import keymanagement
-import message
-import math
-import ownbase32
+import vernam.keymanagement
+import vernam.message
+import vernam.ownbase32
 
 
-def encrypt(inputPath,keyPath,outputPath, force=False, mode="raw"):
+def encrypt(inputPath, keyPath, outputPath, force=False, mode="raw"):
     """
     This function performs vernam encryption using an input file and a key,
     result is stored in output file.
@@ -29,23 +33,23 @@ def encrypt(inputPath,keyPath,outputPath, force=False, mode="raw"):
     if os.path.exists(outputPath):
         if force is False:
             sys.exit("output file exists, won't overwrite {}".format(
-                                                            outputPath))
+                outputPath))
         else:
             sys.stderr.write("Output file will be overwritten as requested.\n")
     if not os.path.exists(keyPath):
         sys.exit("key file not found\n")
     try:
         if mode == "lz4":
-            inputfile=bytearray(compressHC((open(inputPath, 'rb')).read()))
+            inputfile = bytearray(compressHC((open(inputPath, 'rb')).read()))
         elif mode == "human":
-            inputfile=ownbase32.string2ownBase32(open(inputPath,'rb').read())
+            inputfile = ownbase32.string2ownBase32(open(inputPath, 'rb').read())
         else:
-            inputfile=bytearray(open(inputPath, 'rb').read())
+            inputfile = bytearray(open(inputPath, 'rb').read())
     except:
         raise
     if mode == "human":
         # An even number of bytes is needed for tranformation.
-        size =  int(math.ceil(((len(inputfile) / 3)/2.)) * 2)
+        size = int(math.ceil(((len(inputfile) / 3)/2.)) * 2)
     else:
         size = len(inputfile)
 
@@ -56,13 +60,13 @@ def encrypt(inputPath,keyPath,outputPath, force=False, mode="raw"):
         if len(key) != len(inputfile):
             key = key[:len(inputfile)-len(key)]
         inputfile = ownbase32.string2ob32ba(inputfile)
-        ciphered = vernam(inputfile,key)
-        message.writeHumanMessage(outputPath,ciphered,offset * 3)
+        ciphered = vernam(inputfile, key)
+        message.writeHumanMessage(outputPath, ciphered, offset * 3)
     else:
-        message.writeMessage(keyPath,outputPath,vernam(inputfile, key), offset,
-                        l2r=l2r)
+        message.writeMessage(keyPath, outputPath, vernam(inputfile, key), offset,
+                             l2r=l2r)
 
-def decrypt(inputPath,keyPath,outputPath, force=False, mode="raw"):
+def decrypt(inputPath, keyPath, outputPath, force=False, mode="raw"):
     """
     This function performs vernam decryption using an input file and a key,
     result is stored in output file.
@@ -82,32 +86,32 @@ def decrypt(inputPath,keyPath,outputPath, force=False, mode="raw"):
     if os.path.exists(outputPath):
         if force is False:
             sys.exit("output file exists, won't overwrite {}".format(
-                                                            outputPath))
+                outputPath))
         else:
             sys.stderr.write("Output file will be overwritten as requested.\n")
     if mode == "human":
-        seek,inputCryp = message.readHumanMessage(inputPath)
+        seek, inputCryp = message.readHumanMessage(inputPath)
         offset = seek / 3
         size = int(math.ceil(((len(inputCryp) / 3)/2.)) * 2)
         l2r = True
     else:
-        offset, l2r, inputCryp = message.readMessage(keyPath,inputPath)
-        offset=offset[0]
-        size=len(inputCryp)
-    print("offset: {} - {}, l2r: {}".format(offset,type(offset), l2r))
+        offset, l2r, inputCryp = message.readMessage(keyPath, inputPath)
+        offset = offset[0]
+        size = len(inputCryp)
+    print("offset: {} - {}, l2r: {}".format(offset, type(offset), l2r))
     key = keymanagement.getKeyBytes(keyPath, size, offset=offset, l2r=l2r)
-    key=bytearray(key[0])
+    key = bytearray(key[0])
     if mode == "human":
         key = keymanagement.ba2humankeyba(key)
         if len(key) != len(inputCryp):
             key = key[:len(inputCryp)-len(key)]
         inputCryp = ownbase32.string2ob32ba(inputCryp)
-        clear = ownbase32.ba2ob32string(vernam(inputCryp,key))
+        clear = ownbase32.ba2ob32string(vernam(inputCryp, key))
         print(clear)
     else:
-        clear = vernam(bytearray(inputCryp),key)
+        clear = vernam(bytearray(inputCryp), key)
     if mode == "lz4":
-        clear=uncompress(str(clear))
+        clear = uncompress(str(clear))
     open(outputPath, 'wb').write(clear)
 
 
@@ -116,16 +120,15 @@ def vernam(one, two):
     This function performs vernam (de)ciphering with two bytearrays, order
     doesn't matter as it is a pure xor fuction. function will return xored
     bytearray
-
+ 
     parameters
     ----------
     one, two : Two arraybytes to perform vernam cipher.
     """
     if len(one) != len(two):
         sys.exit("arraybytes length differs, can not vernam with them")
-    size=len(one)
+    size = len(one)
     outputfile = bytearray(size)
     for i in range(size):
         outputfile[i] = one[i] ^ two[i]
     return outputfile
-
